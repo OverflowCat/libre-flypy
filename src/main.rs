5d6895226c6947f42fe37342a023e90a55a00cb7
猫猫 <overflowcat@gmail.com>
2023-07-03 05:19:58 +0800

    temp2

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,62 @@
-pub mod flypy;
+mod ids;
+mod roots;
 
-use pinyin::{ToPinyin, ToPinyinMulti};
 use std::{
-    collections::VecDeque,
-    fs::File,
+    collections::HashMap,
     io::{BufRead, BufReader},
 };
 
-struct Structure {
-    symbol: char,
-    count: u8,
-}
+use ids::Tree;
 
-impl Structure {
-    fn new(symbol: char, count: u8) -> Structure {
-        Structure { symbol, count }
+pub fn parse_file() {
+    let (mut firsts, mut lasts) = roots::generate();
+    let mut done: HashMap<char, String> = HashMap::new();
+    let mut todos: Vec<(char, Tree)> = Vec::new();
+    // read ./ids/ids_lv2.txt line by line
+    let file = std::fs::File::open("./ids/ids_lv2.txt").expect("File not found.");
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.expect("Line not found.");
+        let (character, sequence) = ids::parse_ids(&line);
+        todos.push((character, sequence));
     }
+    let mut prev_len = 0;
+    loop {
+        todos = todos
+            .into_iter()
+            .filter_map(|(character, tree)| {
+                let first_part = tree.get_first_leaf();
+                let last_part = tree.get_last_leaf();
+                let first_code = firsts.get(&first_part);
+                let last_code = lasts.get(&last_part);
+                if first_code.is_some() && last_code.is_some() {
+                    let first_code = first_code.unwrap();
+                    let last_code = last_code.unwrap();
+                    println!(
+                        "{}: {}{} {}{}",
+                        character, first_part, last_part, first_code, last_code
+                    );
+                    done.insert(character, format!("{}{}", first_code, last_code));
+                    firsts.insert(character, *first_code);
+                    lasts.insert(character, *last_code);
+                    None
+                } else {
+                    // println!("{}: {}{} not found", character, first_part, last_part);
+                    Some((character, tree))
+                }
+            })
+            .collect();
+        let curr_len = todos.len();
+        if curr_len == prev_len {
+            break;
+        }
+        prev_len = curr_len;
+    }
+    println!("{:?}", done);
+    println!("总计: {}", prev_len);
 }
 
 fn main() {
-    ids();
+    println!("Hello, world!");
+    parse_file();
 }
-
-fn ids() {
-    let file = File::open("ids/ids_lv2.txt").unwrap();
-    let reader = BufReader::new(file);
-
-    let mut total = 0;
-    let mut parsed = 0;
-    reader.lines().for_each(|line| match line {
-        Ok(entry) => {
-            let res = parse(&entry);
-            if res.len() > 3 {
-                let mut flag = false;
-                for 多音 in res.chars().next().unwrap().to_pinyin_multi() {
-                    for 音 in 多音 {
-                        flag = true;
-                        print!("{} ", 音.with_tone_num());
-                    }
-                }
-                if flag {
-                    parsed += 1;
-                }
-                println!("{}", res);
-            }
-            total += 1;
-        }
-        Err(msg) => {
-            println!("Err: {:?}", msg);
-            total += 1;
-        }
-    });
-    println!("Parsed {} entries in {}.", parsed, total);
-}
-
-fn parse(entry: &str) -> String {
-    let structures = vec![
-        Structure::new('⿰', 2),
-        Structure::new('⿱', 2),
-        Structure::new('⿳', 3),
-    ];
-
-    let mut columns: VecDeque<&str> = entry.split('\t').collect();
-    if columns.len() < 2 {
-        return "".to_string();
-    }
-    let character = columns.pop_front().unwrap();
-    let v: Vec<String> = columns
-        .iter()
-        .filter_map(|&s| {
-            let s = if s.contains(";") {
-                let v: Vec<&str> = s.split(";").collect();
-                v[0]
-            } else {
-                s
-            };
-            let chars: Vec<char> = s.chars().collect();
-            structures
-                .iter()
-                .any(|structure| {
-                    chars[0] == structure.symbol && chars.len() == structure.count as usize + 1
-                })
-                .then_some(s.to_string())
-        })
-        .collect();
-    if v.len() > 0 {
-        format!("{}: {}", character, v.join(" / "))
-    } else {
-        "".to_string()
-    }
-}
-
-/* fn pinyin_to_pbyb(p: &str) -> String {
-    let mut pbyb = String::new();
-    let 声母 = if p.starts_with("sh") {
-        'u'
-    } else if p.starts_with("ch") {
-        'i'
-    } else if p.starts_with("zh") {
-        'v'
-    } else {
-        p.chars().next().unwrap()
-    };
-}
- */
